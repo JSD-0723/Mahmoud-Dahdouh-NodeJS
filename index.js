8e3a40b695c5efa6386e34add7097cb8de5d32ac
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-08-30 11:14:30 +0300

    create logRequest function

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const host = "localhost"
 const port = 8000
 
 const requestListener = function (req, res) {
+  // Log request
+  logRequest(req.url)
+
   res.statusCode = 200
   res.setHeader("Content-Type", "text/plain")
   res.end("Hello World!")
 }
 const server = http.createServer(requestListener)
+
+// Log request
+// save time and URL to 'requests.txt' file
+const logRequest = async (url) => {
+  if (url === "/favicon.ico") return
+  try {
+    await fs.appendFile("requests.txt", `${new Date().toISOString()} ${url}\n`)
+    console.log("done", url)
+  } catch (error) {
+    console.error(
+      `Got an error while trying to write to a file: ${error.message}`
+    )
+  }
+}
 
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`)
