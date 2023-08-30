601eb74d2f0a28b1c8240e05f9a4552b9379dadd
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-08-30 11:13:05 +0300

    setup NodsJS server

---
+++ index.js
@@ -0,0 +1,16 @@
+import http from "http"
+import fs from "fs/promises"
+
+const host = "localhost"
+const port = 8000
+
+const requestListener = function (req, res) {
+  res.statusCode = 200
+  res.setHeader("Content-Type", "text/plain")
+  res.end("Hello World!")
+}
+const server = http.createServer(requestListener)
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`)
+})
